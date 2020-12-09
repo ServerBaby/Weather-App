2f1657a17ad81a106275f98cd545a60bf84472ef
ServerBaby <julie@selascreations.com>
2020-12-10 03:58:16 +1100

    Add files via upload

---
+++ weather_app_photo.py
@@ -0,0 +1,32 @@
+import requests # to get image from the web
+import shutil   # to save file locally
+import base64   # to convert the jpg to base64
+
+# URL of the image being used:
+image_url = "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/041529/041529_045.jpg"
+
+# Retrieve the url image; use stream = True to guarantee no interruptions.
+r = requests.get(image_url, stream = True)
+
+# use slice notation to separate the filename from the image link
+filename = image_url.split("/")[-1]
+
+# Check if the image was retrieved successfully
+if r.status_code == 200:
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    r.raw.decode_content = True
+
+    # Create a local file with wb ( write binary ) permission.
+    with open(filename,'wb') as f:
+        shutil.copyfileobj(r.raw, f)    
+    f.close() 
+    # Open file with rb ( read binary ) permission.
+    with open(filename, "rb") as f:
+        # encode the data in the file into base64 
+        the_image = base64.b64encode(f.read())
+    # To print the base64 without the [b'   '], decode the string to utf-8 format
+    print(the_image.decode('utf-8'))
+    f.close() 
+else:
+    # in case the image isn't retrieved successfully:
+    print('Image Couldn\'t be retrieved')
