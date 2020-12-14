8115ce38f9d03b6f5031683f582e74e40517b1de
ServerBaby <julie@selascreations.com>
2020-12-14 14:35:19 +1100

    Update image_url.py
    Updated image_url with more comments and new class/method/variable names.

--- image_url.py
+++ image_url.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from dl_jpg_to_b64_module import *
+import dl_img_conv_b64
 
-# URL of the image being used:
+# URL of the image being used (Can be changed to whatever you'd like)
 image_url = "https://weathercams.airservicesaustralia.com/wp-content/uploads/airports/041529/041529_045.jpg"
 
-dl_jpg_to_b64(image_url)
+# using the method "conv_img_to_b64" from the "DownloadConvert" class, download an image and convert it to base64
+output = dl_img_conv_b64.DownloadConvert().conv_img_to_b64(image_url)
+
+print(output)   # print that base64 code to the console
