395bcd8d0ee5e7821c7d14cc9ebe8e12360fee80
ServerBaby <julie@selascreations.com>
2020-12-16 11:41:24 +1100

    Add files via upload

---
+++ wellcamp_urls.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+""" Toowoomba Wellcamp Airport URLs
+
+This file is just a list of relevant URLs for use in the weather app.
+Specifically, this file contains the URLs for the data from
+Toowoomba Wellcamp Airport
+
+Description of Toowoomba Wellcamp Airport for interest:
+
+From Wikipedia: Toowoomba Wellcamp Airport (IATA: WTB, ICAO: YBWW) is
+an airport in Wellcamp, 8.4 nautical miles (15.6 km; 9.7 mi) west from
+the CBD of Toowoomba, Queensland, Australia. It was known as Brisbane
+West Wellcamp Airport until November 2017.
+
+Time zone: UTC+10:00 ()
+Elevation AMSL: 1,509 ft / 460 m
+Coordinates: 27°33′30″S 151°47′36″E
+Website www.wellcamp.com.au
+"""
+
+# URL for BOM weather data:
+data_url = "http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.99435.json"
+
+# URL of the image being used:
+image_url = "https://weathercams.airservicesaustralia.com/wp-content/" \
+            "uploads/airports/041529/041529_045.jpg"
+
