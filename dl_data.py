8bcb3e6bc5af9d678566af2e3f6a5e4d5269f3d8
ServerBaby <julie@selascreations.com>
2021-02-15 09:42:09 +1100

    Update dl_data.py
    removed some converting to strings and back again

--- dl_data.py
+++ dl_data.py
@@ -2,66 +2,40 @@
 
 """Download and manipulate BOM Data
 
-This script allows the user to download the most recently available
-weather conditions for a specific location from the Bureau of
-Meteorology (BOM) website, manipulate that data including adding
-a base64 encoded image of the location at that time, and then converts
-that data back into json format.
-
-Currently being tested on data from Toowoomba Wellcamp Airport.
-`http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.99435.json`
-
-This script requires that several pypi modules be installed within the
-Python environment you are running this script in.  These modules are
-`requests` to access online content, `json` to convert data to and from
-json format, and `datetime` to change how dates & times are displayed.
-
-This script also requires two accompanying custom modules be installed.
-These modules are: `dl_img_conv_b64` (to download an image and convert
-to base64) and `wellcamp_urls` (containing the links to the most recent
-data and image from the target location, Wellcamp Airport).
-
 The dl_data python file can be imported as a module and contains the
 following functions:
 
     * __init__ - to construct the main function
     * dl_weather - returns the data as a dictionary in JSON format
-    * dl_time - extracts a human-readable version of the local time
+    * dl_time_local - extracts a human-readable version of the local time
         from the BOM data
+    * dl_time_utc - extracts the time from the BOM data and expresses it
+        as seconds from epoch.
 """
 
 import requests
 import json
 import datetime
+import time
 import dl_img_conv_b64
 from wellcamp_urls import image_url, data_url
 
 
-class DownloadData:
+class DownloadData:   # Output in json format. ???????
     """
-    A class to download the most recent weather conditions at a
-    specific location.  Output in json format.
-
-    ...
+    Output in json format. ???????
 
     Attributes
     ----------
-    'data_url' : str
-        The URL of the data to be downloaded from the BOM website
-        Defined outside of the scope of the class.
-    'image_url' : str
-        The URL of the image to be downloaded from AirServiceAus site
-        Defined outside of the scope of the class.
+    'data_url' : str -  The URL of the data to be downloaded from the BOM website
+    'image_url' : str - The URL of the image to be downloaded from AirServiceAus site
 
     Methods
     -------
     'dl_weather(data_url, image_url)'
-        Downloads the most recent weather conditions at a set location
-        as a dictionary, adds to that data the base64 representation of
-        an image that is the current view of that location.
+        Downloads the most recent weather conditions at a set location as a dictionary
      'dl_time(data_url)'
-        Extracts the time that the most recent weather conditions data
-        was uploaded and returns it in a neat, human readable format.
+        Extracts the time that the most recent weather conditions data was uploaded
     """
 
     def __init__(self):
@@ -73,47 +47,6 @@ class DownloadData:
         pass
 
     def dl_weather(self, data_url, image_url):
-        """
-        Method called upon to create a dictionary of current weather
-        conditions at a specific location.
-        This method downloads the data from the BOM website, extracts
-        the required data and places it into a dictionary, downloads
-        an image from AirServices Australia, converts that image into
-        a base64 encoded string, and then adds that string to the
-        dictionary.
-
-        ...
-
-        Parameters
-        ----------
-        'data_url' : str
-           The URL of the data to be downloaded from the BOM website
-           Defined outside of the scope of the class.
-        'image_url' : str
-            The URL of the image to be downloaded from AirServiceAus site
-            Defined outside of the scope of the class.
-
-
-        `returned_object` : file-like object
-            Object containing the image URL server's response to the HTTP
-            GET request.  `raw` is used to create a file-like object
-            representation of the response. Use of raw requires that
-            stream=True be set on the request. stream=True guarantees no
-            interruptions while downloading the image, avoiding corruption
-            of the object being downloaded. Setting decode_content=True
-            forces the decompression of the response.raw file-like object.
-
-        `result` : str [in utf-8 format]
-            The string representation of the base64 encoded image.
-            Encoded into utf-8 format to return string without the [b' '].
-
-        Raises
-        ------
-        status.code if/else statement:
-            Checks if image was retrieved successfully (HTTP Status = 200).
-            If image download fails, (HTTP status code != 200), an error
-            message is printed in the console.
-        """
 
         x = requests.get(data_url)
 
@@ -123,57 +56,71 @@ class DownloadData:
         if x.status_code == 200:
 
             y = json.loads(x.text)["observations"]["data"][0]
+
+            # converts utc time to seconds since epoch
+            y.update(epoch_date=1000*int(eval((self.dl_time_utc(data_url)))))
+
             # adds base64 image to data
             y.update(local_image_b64=dl_img_conv_b64.DownloadConvert().conv_img_to_b64(image_url))
+
             # Removes the unnecessary "sort order" value;
             y.pop("sort_order", None)
 
-            result = str(y)
+            result = y
 
         else:
             result = str('Data Couldn\'t be retrieved')
 
         return result
 
-    def dl_time(self, data_url):
+    def dl_time_local(self, data_url):
         # defined in previous function
         x = requests.get(data_url)
         y = json.loads(x.text)["observations"]["data"][0]["local_date_time_full"]
 
-        # Creates a heading for displaying the current dataset with it's time as part of the heading
-        t1 = datetime.datetime.strptime(y, "%Y%m%d%H%M%S")
-        t2 = datetime.datetime.strftime(t1, '%A, %d %B %Y %H:%M:%S')
+        # Creates a heading for displaying the current dataset with its time as part of the heading
+        dd = datetime.datetime
+        t_strp_local = dd.strptime(y, "%Y%m%d%H%M%S")
+        t_local = dd.strftime(t_strp_local, '%A, %d %B %Y %H:%M:%S')
 
         # prints the local time that the data was uploaded to the BOM website
-        return t2
+        return t_local
+
+    def dl_time_utc(self, data_url):
+        # defined in previous function
+        x = requests.get(data_url)
+        z = json.loads(x.text)["observations"]["data"][0]["aifstime_utc"]
+
+        # Creates a heading for displaying the current dataset with its time as part of the heading
+        dd = datetime.datetime
+        t_strp_utc = dd.strptime(z, "%Y%m%d%H%M%S")
+        t_tuple_utc = dd.timetuple(t_strp_utc)
+        t_utc = time.mktime(t_tuple_utc)
+        # prints the local time that the data was uploaded to the BOM website
+        return str(t_utc)
 
 
 if __name__ == "__main__":
-    """ 
-    Function to test the functions in this module
-    Uses the method conv_img_to_b64 from the class DownloadConvert to
-    downloads and convert a sample image into a utf-8 encoded base64
-    string.  This string is then printed to the console/terminal.
-    This code will only run if the module is run directly (not imported
-    by another module).  It can be used as a unit test for this module.
 
-    ...
+    output_time_local = DownloadData().dl_time_local(data_url)
+    output_time_utc = (DownloadData().dl_time_utc(data_url))
 
-    Attributes
-    ----------
-    image_url : str
-        URL of the sample image to be downloaded. 
-    output: str
-        The string representation of the base64 encoded sample image.
-        Encoded into utf-8 format to return string without the [b' '].
+    # Print local time in custom format:
+    print(output_time_local)
+    # Print utc time in seconds since epoch format:
+    print(output_time_utc)
+    # Print utc time in a standard format (to provide a visual confirmation of correct conversion)
+    print(time.asctime(time.gmtime(float(output_time_utc))))
 
-    Methods
-    -------
-    dl_weather(data_url)
-        Downloads an online image, then converts it to Base64 and then 
-        returns the result.
-    """
-
+    # To check if new fields were added correctly:
     output_weather = DownloadData().dl_weather(data_url, image_url)
-    output_time = DownloadData().dl_time(data_url)
-    print(output_time + "\n" + output_weather)
+    # Option to print all of the data as a dictionary:
+    print(output_weather)
+    # Confirm it is actually a dictionary, not a string:
+    print(type(output_weather))
+
+    # to check the the types of the values in the dictionary:
+    for key in output_weather:
+        print(key, ', ', type(output_weather[key]))
+    # to include the downloaded values with the key and key type:
+        # print(key, ', ', type(output_weather[key]), ', ', output_weather[key])
